61fbf6b2e2067ff10ada8747b38d8a1c22a74c68
vunguyen12 <54542419+vunguyen12@users.noreply.github.com>
2019-09-03 09:40:55 -0500

    newest

---
+++ src/GetUserInfo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class GetUserInfo {
+    public static  void main(String[] args){
+     String name;
+     int age;
+     Scanner inputDevice = new Scanner(System.in);
+     System.out.println("please enter your name >>> ");
+     name = inputDevice.nextLine();
+
+     System.out.println("please enter your age >>> ");
+     age = inputDevice.nextInt();
+
+     System.out.println(" your name is " + name+ " and you are " + age + " years old");
+
+
+    }
+}
---
+++ src/SalaryDialog.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+public class SalaryDialog {
+    public static void main(String[] args) {
+        String wageString;
+        String dependentString;
+        int dependents;
+        double weeklyPay;
+        final double HOURS_IN_WEEK = 37.5;
+        boolean isYes;
+        int selection;
+
+        wageString = JOptionPane.showInputDialog(null, "enter employee's hourly wage",
+                "Salary Dialog 1", JOptionPane.INFORMATION_MESSAGE);
+        weeklyPay = Double.parseDouble(wageString) * HOURS_IN_WEEK;
+        dependentString = JOptionPane.showInputDialog(null, "How many dependents?",
+                "Salary Dialog 2 ", JOptionPane.QUESTION_MESSAGE);
+        dependents = Integer.parseInt(dependentString);
+        JOptionPane.showMessageDialog(null, " Weekly salary is $" +
+        weeklyPay + "\nDeduction will be made for " + dependents +" dependents");
+        selection = JOptionPane.showConfirmDialog(null," is your salary correct");
+        isYes = (selection == JOptionPane.YES_OPTION);
+        JOptionPane.showMessageDialog(null,"you responded " + isYes);
+    }
+}
